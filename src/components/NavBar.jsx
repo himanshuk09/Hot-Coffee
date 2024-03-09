dc714b2e09ec02467366687bd4c72a2a7c0929b9
Himanshu Khade <20013himanshu@gmail.com>
2024-03-09 08:05:38 +0530

    update navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,17 +6,7 @@ const NavBar = () => {
       <Link to="/" className="logo">
       {" "}
       Hot Coffee<i className="fas fa-mug-hot"></i>{" "}
-      </Link>
-
-      {/**<nav className="navbar">
-        <a href="#home">home</a>
-        <a href="#about">about</a>
-        <a href="#menu">menu</a>
-        <a href="#review">review</a>
-        <a href="#book">book</a>
-      </nav> */}
-
-      
+      </Link>  
       <Link to="/menu" className="btn">Order Now</Link>
     </header>
   );
